997c3b7709bfa342dcd7b9123d403e30a6ad9590
David Williams <thedavesignal@gmail.com>
2017-11-07 16:00:27 -0800

    Implement a rails-y user creation form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    #@user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :email, :password, :authenticity_token)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107000015) do
+ActiveRecord::Schema.define(version: 20171107012114) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
